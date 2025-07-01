8b30c0bea5927482d085d8304951f22260f59985
“Amin <abehnami@berkeley.edu>
2025-06-30 23:44:41 -0700

    Exact ex2 code for autograder

--- lab01/ex2_pointers_and_functions.c
+++ lab01/ex2_pointers_and_functions.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 void add_one(int input) {
-  input += 1;
+    input += 1;
 }
 
 void add_one_pointer(int* input) {
-  *input += 1;
+    *input += 1;
 }
 
 void add_one_double_ptr(int** input) {
-  **input += 1;
+    **input += 1;
 }
 
 int main() {
-  int x = 5;
+    int x = 5;
 
-  add_one(x);
-  printf("After add_one: %d\n", x);
+    add_one(x);
+    printf("After add_one: %d\n", x);
 
-  add_one_pointer(&x);
-  printf("After add_one_pointer: %d\n", x);
+    add_one_pointer(&x);
+    printf("After add_one_pointer: %d\n", x);
 
-  int* ptr = &x;
-  add_one_double_ptr(&ptr);
-  printf("After add_one_double_ptr: %d\n", x);
+    int* ptr = &x;
+    add_one_double_ptr(&ptr);
+    printf("After add_one_double_ptr: %d\n", x);
 
-  return 0;
+    return 0;
 }

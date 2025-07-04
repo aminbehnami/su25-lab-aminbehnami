69b3c39ed8bb341a502dd492c3c90752e06cabe7
aminbehnami <abehnami@berkeley.edu>
2025-07-04 14:14:08 -0700

    Update pwd_checker.c

--- lab02/pwd_checker.c
+++ lab02/pwd_checker.c
@@ -2,6 +2,23 @@
 #include <stdbool.h>
 #include "pwd_checker.h"
 
+/*
+Password checker
+
+Requirements:
+- Password must be at least 10 characters
+- Password must contain at least
+    - 1 upper case letter
+    - 1 lower case letter
+    - 1 number
+- Password cannot contain the person's first name or last name (case sensitive)
+
+For the simplicity of this exercise:
+- This is not the most efficient way to implement this program
+- These functions do not perform any error checking
+- You can assume that the first and last name will never be the empty string
+*/
+
 bool check_length(const char *password) {
     return strlen(password) >= 10;
 }
@@ -11,25 +28,31 @@ bool check_range(char letter, char lower, char upper) {
 }
 
 bool check_upper(const char *password) {
-    while (*password) {
-        if (check_range(*password, 'A', 'Z')) return true;
-        password++;
+    while (*password != '\0') {
+        if (check_range(*password, 'A', 'Z')) {
+            return true;
+        }
+        ++password;
     }
     return false;
 }
 
 bool check_lower(const char *password) {
-    while (*password) {
-        if (check_range(*password, 'a', 'z')) return true;
-        password++;
+    while (*password != '\0') {
+        if (check_range(*password, 'a', 'z')) {
+            return true;
+        }
+        ++password;
     }
     return false;
 }
 
 bool check_number(const char *password) {
-    while (*password) {
-        if (check_range(*password, '0', '9')) return true;
-        password++;
+    while (*password != '\0') {
+        if (check_range(*password, '0', '9')) {
+            return true;
+        }
+        ++password;
     }
     return false;
 }

0ec89f78b17be44d61021df7a735388b1b3e7503
aminbehnami <abehnami@berkeley.edu>
2025-07-04 14:07:47 -0700

    Update test_pwd_checker.c

--- lab02/test_pwd_checker.c
+++ lab02/test_pwd_checker.c
@@ -11,7 +11,7 @@ bool check_range(char letter, char lower, char upper) {
 }
 
 bool check_upper(const char *password) {
-    while (*password) {
+    while (*password != '\0') {
         if (check_range(*password, 'A', 'Z')) return true;
         password++;
     }
@@ -19,7 +19,7 @@ bool check_upper(const char *password) {
 }
 
 bool check_lower(const char *password) {
-    while (*password) {
+    while (*password != '\0') {
         if (check_range(*password, 'a', 'z')) return true;
         password++;
     }
@@ -27,7 +27,7 @@ bool check_lower(const char *password) {
 }
 
 bool check_number(const char *password) {
-    while (*password) {
+    while (*password != '\0') {
         if (check_range(*password, '0', '9')) return true;
         password++;
     }
@@ -39,9 +39,11 @@ bool check_name(const char *first_name, const char *last_name, const char *passw
 }
 
 bool check_password(const char *first_name, const char *last_name, const char *password) {
-    return check_length(password) &&
-           check_upper(password) &&
-           check_lower(password) &&
-           check_number(password) &&
-           check_name(first_name, last_name, password);
+    bool lower = check_lower(password);
+    bool length = check_length(password);
+    bool name = check_name(first_name, last_name, password);
+    bool number = check_number(password);
+    bool upper = check_upper(password);
+
+    return lower && length && name && number && upper;
 }

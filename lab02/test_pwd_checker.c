fd8f4467144d4228eaee10eb2d5e12578e3a3355
aminbehnami <abehnami@berkeley.edu>
2025-07-04 14:12:52 -0700

    Update test_pwd_checker.c

--- lab02/test_pwd_checker.c
+++ lab02/test_pwd_checker.c
@@ -12,57 +12,50 @@ Requirements:
     - 1 lower case letter
     - 1 number
 - Password cannot contain the person's first name or last name (case sensitive)
+
+For the simplicity of this exercise:
+- This is not the most efficient way to implement this program
+- These functions do not perform any error checking
+- You can assume that the first and last name will never be the empty string
 */
 
-/* Returns true if the length of PASSWORD is at least 10, false otherwise */
 bool check_length(const char *password) {
     return strlen(password) >= 10;
 }
 
-/* Returns true if LETTER is in the inclusive range [LOWER, UPPER] */
 bool check_range(char letter, char lower, char upper) {
     return letter >= lower && letter <= upper;
 }
 
-/* Returns true if PASSWORD contains at least one upper case letter */
 bool check_upper(const char *password) {
-    while (*password) {
-        if (check_range(*password, 'A', 'Z')) return true;
-        password++;
+    while (*password != '\0') {
+        if (check_range(*password, 'A', 'Z')) {
+            return true;
+        }
+        ++password;
     }
     return false;
 }
 
-/* Returns true if PASSWORD contains at least one lower case letter */
 bool check_lower(const char *password) {
-    while (*password) {
-        if (check_range(*password, 'a', 'z')) return true;
-        password++;
+    while (*password != '\0') {
+        if (check_range(*password, 'a', 'z')) {
+            return true;
+        }
+        ++password;
     }
     return false;
 }
 
-/* Returns true if PASSWORD contains at least one number */
 bool check_number(const char *password) {
-    while (*password) {
-        if (check_range(*password, '0', '9')) return true;
-        password++;
+    while (*password != '\0') {
+        if (check_range(*password, '0', '9')) {
+            return true;
+        }
+        ++password;
     }
     return false;
 }
 
-/* Returns true if the person's first and last name are NOT in the password */
 bool check_name(const char *first_name, const char *last_name, const char *password) {
-    return strstr(password, first_name) == NULL && strstr(password, last_name) == NULL;
-}
-
-/* Returns true if PASSWORD meets all criteria */
-bool check_password(const char *first_name, const char *last_name, const char *password) {
-    bool length = check_length(password);
-    bool upper = check_upper(password);
-    bool lower = check_lower(password);
-    bool number = check_number(password);
-    bool name = check_name(first_name, last_name, password);
-
-    return length && upper && lower && number && name;
-}
+    return strstr(password, first_name) == NULL && strstr(password, last_name) == NULL

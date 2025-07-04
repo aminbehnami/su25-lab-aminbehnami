48add02694fa8dc30991a3bace0b1f8d8ce205ed
aminbehnami <abehnami@berkeley.edu>
2025-07-04 14:46:34 -0700

    Update pwd_checker.h

--- lab02/pwd_checker.h
+++ lab02/pwd_checker.h
@@ -3,6 +3,12 @@
 
 #include <stdbool.h>
 
+bool check_range(char letter, char lower, char upper);
+bool check_upper(const char *password);
+bool check_lower(const char *password);
+bool check_number(const char *password);
+bool check_length(const char *password);
+bool check_name(const char *first_name, const char *last_name, const char *password);
 bool check_password(const char *first_name, const char *last_name, const char *password);
 
-#endif // PWD_CHECKER_H
+#endif

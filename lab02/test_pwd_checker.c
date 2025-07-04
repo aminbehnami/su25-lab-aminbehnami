ab1fa9600fce1b90b9e96c0e0c90fc65416358bc
aminbehnami <abehnami@berkeley.edu>
2025-07-04 14:15:08 -0700

    Update test_pwd_checker.c

--- lab02/test_pwd_checker.c
+++ lab02/test_pwd_checker.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "pwd_checker.h"
 
 int main() {
@@ -6,10 +7,10 @@ int main() {
 
     const char *test1_first = "Abraham";
     const char *test1_last = "Garcia";
-    const char *test1_pwd = "Zyx987!@#q";  // Meets all rules
+    const char *test1_pwd = "Zyx987!@#q";  // ✅ Valid: 10+ chars, upper, lower, number, no names
 
     bool test1 = check_password(test1_first, test1_last, test1_pwd);
-    printf("Test1 result: %d\n", test1);  // Should print 1 (true)
 
+    printf("Test1 result: %d\n", test1);  // Should be 1 (true)
     return 0;
 }

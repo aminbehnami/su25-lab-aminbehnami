157a384ebfbb0c3ae8d55b7140d2fdb1a4bb677c
aminbehnami <abehnami@berkeley.edu>
2025-07-03 23:36:54 -0700

    Update ex1_compiler_warnings.c

--- lab02/ex1_compiler_warnings.c
+++ lab02/ex1_compiler_warnings.c
@@ -10,14 +10,12 @@ struct Course {
 struct Course *make_course(int id, char *name) {
     struct Course *new_course = malloc(sizeof(struct Course));
     new_course->id = id;
-    new_course->name = name;  // or strdup(name) if needed
-
+    new_course->name = name;
     return new_course;
 }
 
 int main() {
     struct Course *cs161 = make_course(161, "Computer Security");
     printf("Welcome to CS%d: %s!\n", cs161->id, cs161->name);
-
     return 0;
 }

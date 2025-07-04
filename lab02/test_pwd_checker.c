2c86b8c478526c693264f2f7f1f7a93346d395bc
aminbehnami <abehnami@berkeley.edu>
2025-07-04 14:13:35 -0700

    Update test_pwd_checker.c

--- lab02/test_pwd_checker.c
+++ lab02/test_pwd_checker.c
@@ -1,61 +1,15 @@
-#include <string.h>
-#include <stdbool.h>
+#include <stdio.h>
 #include "pwd_checker.h"
 
-/*
-Password checker
+int main() {
+    printf("Running tests...\n\n");
 
-Requirements:
-- Password must be at least 10 characters
-- Password must contain at least
-    - 1 upper case letter
-    - 1 lower case letter
-    - 1 number
-- Password cannot contain the person's first name or last name (case sensitive)
+    const char *test1_first = "Abraham";
+    const char *test1_last = "Garcia";
+    const char *test1_pwd = "Zyx987!@#q";  // Meets all rules
 
-For the simplicity of this exercise:
-- This is not the most efficient way to implement this program
-- These functions do not perform any error checking
-- You can assume that the first and last name will never be the empty string
-*/
+    bool test1 = check_password(test1_first, test1_last, test1_pwd);
+    printf("Test1 result: %d\n", test1);  // Should print 1 (true)
 
-bool check_length(const char *password) {
-    return strlen(password) >= 10;
+    return 0;
 }
-
-bool check_range(char letter, char lower, char upper) {
-    return letter >= lower && letter <= upper;
-}
-
-bool check_upper(const char *password) {
-    while (*password != '\0') {
-        if (check_range(*password, 'A', 'Z')) {
-            return true;
-        }
-        ++password;
-    }
-    return false;
-}
-
-bool check_lower(const char *password) {
-    while (*password != '\0') {
-        if (check_range(*password, 'a', 'z')) {
-            return true;
-        }
-        ++password;
-    }
-    return false;
-}
-
-bool check_number(const char *password) {
-    while (*password != '\0') {
-        if (check_range(*password, '0', '9')) {
-            return true;
-        }
-        ++password;
-    }
-    return false;
-}
-
-bool check_name(const char *first_name, const char *last_name, const char *password) {
-    return strstr(password, first_name) == NULL && strstr(password, last_name) == NULL

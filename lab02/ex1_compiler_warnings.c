daab880fc9639643f62cdd8773127e8bd0434ab0
“Amin <abehnami@berkeley.edu>
2025-07-03 23:02:29 -0700

    Restore original lab02 files from starter repo

--- lab02/ex1_compiler_warnings.c
+++ lab02/ex1_compiler_warnings.c
@@ -1,21 +1,22 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <stdlib.h>  // added for malloc
+#include <string.h>
 
 struct Course {
-    char* name;
-    int number;
+    int id;
+    char *name;
 };
 
-// Takes in a name and a number and returns a pointer to a Course struct
-struct Course* make_course(char* name, int number) {
-    struct Course* new_course = malloc(sizeof(struct Course));
-    new_course->name = name;         // fixed: removed dereference
-    new_course->number = number;
-    return new_course;               // fixed: return the pointer directly
-}
+struct Course *make_course(int id, char *name) {
+    struct Course *new_course = malloc(sizeof(struct Course));
+    new_course->id = id;
+    new_course->name = *name;
 
+    return &new_course;
+}
 int main() {
-    struct Course* cs61c = make_course("CS61C", 61);
-    printf("%s %d\n", cs61c->name, cs61c->number);
+    struct Course *cs161 = make_course(161, "Computer Security");
+    printf("Welcome to CS%d: %s!\n", cs161->id, cs161->name);
+
     return 0;
 }

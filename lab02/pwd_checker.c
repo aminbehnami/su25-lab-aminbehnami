946b103adc339603fb53da7cab651e908a4068c9
aminbehnami <abehnami@berkeley.edu>
2025-07-04 17:13:30 -0700

    Update pwd_checker.c

--- lab02/pwd_checker.c
+++ lab02/pwd_checker.c
@@ -17,8 +17,7 @@ bool check_upper(const char *password) {
     return false;
 }
 
-bool check_lower(const char *password) {
-    while (*password != '\0') {
+bool check_lower(const char *password) {                                              while (*password != '\0') {
         if (check_range(*password, 'a', 'z')) {
             return true;
         }
@@ -37,24 +36,22 @@ bool check_number(const char *password) {
     return false;
 }
 
-bool check_length(const char *password) {
-    // manual counter so `length` is a real local
-    int length = 0;
+bool check_length(const char *password) {                                         int length = 0;
     while (password[length] != '\0') {
         length++;
     }
     return length >= 10;
 }
 
-bool check_name(const char *first_name, const char *last_name, const char *password) {
+bool check_name(const char *first_name, const char *last_name, const char *pass>
     return strstr(password, first_name) == NULL
         && strstr(password, last_name) == NULL;
 }
 
-bool check_password(const char *first_name, const char *last_name, const char *password) {
+bool check_password(const char *first_name, const char *last_name, const char *>
     return check_length(password)
         && check_upper(password)
         && check_lower(password)
         && check_number(password)
         && check_name(first_name, last_name, password);
-}
+}       
